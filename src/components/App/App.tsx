33aaaf55d9f7179e5dfe19f4f58c23c3f9179c72
DESKTOP-P9EK4AV\Tobias Katra <tobias.katra@consid.se>
2023-03-14 11:21:08 +0100

    Initial commit

---
+++ src/components/App/App.tsx
@@ -0,0 +1,13 @@
+import { useReducer, useState } from 'react';
+import './App.scss';
+
+function App () {
+
+  return (
+    <div className="App">
+      <h2>Hello World! :D</h2>
+    </div>
+  );
+}
+
+export default App;
