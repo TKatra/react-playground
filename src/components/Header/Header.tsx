f69d4f5af30c02a1a3fe240d7be399a6b3b989bc
DESKTOP-P9EK4AV\Tobias Katra <tobias.katra@consid.se>
2023-03-15 15:18:11 +0100

    Added a button for an expandable menu

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,14 +4,17 @@ import './Header.scss';
 export default function Header () {
 
   return (
-    <header >
-      <div className='d-flex justify-content-between justify-content-lg-center px-3'>
-        <h1 className='text-center py-3 me-5'>React Playground</h1>
-        <nav className='d-flex align-items-center ms-5'>
-          <Link className='d-inline-block p-3' to={`/`}>Home</Link>
-          <Link className='d-inline-block p-3' to={`/about`}>About</Link>
-        </nav>
-      </div>
+    <header className='d-flex align-items-center justify-content-between justify-content-xl-center ps-3 pe-0 pe-sm-3'>
+      <h1 className='text-center me-0 me-sm-5'>React Playground</h1>
+
+      <nav className='d-none d-sm-block ms-5'>
+        <Link className='d-inline-block p-3' to={`/`}>Home</Link>
+        <Link className='d-inline-block p-3' to={`/about`}>About</Link>
+      </nav>
+
+      <button className='expandable-menu d-sm-none d-block px-3'>
+        <i className="fa-solid fa-bars"></i>
+      </button>
     </header>
   );
 }
